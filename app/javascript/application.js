45870e2783509ae328acfa7512e01da7fcd7f9c6
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2024-01-30 14:03:12 +0000

    updated reservation images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,24 +16,25 @@ setTimeout(() => {
 // const glow = document.getElementById("glow")
 // glow.classList.add("glow")
 
-const swiftUpElements = document.querySelectorAll('.swift-up-text');
-swiftUpElements.forEach(elem => {
+// const swiftUpElements = document.querySelectorAll('.swift-up-text');
+// // swiftUpElements.style.opacity = 0
+// swiftUpElements.forEach(elem => {
 
-	const words = elem.textContent.split(' ');
-	elem.innerHTML = '';
+// 	const words = elem.textContent.split(' ');
+// 	elem.innerHTML = '';
 
-	words.forEach((el, index) => {
-		words[index] = `<span><i>${words[index]}</i></span>`;
-	});
+// 	words.forEach((el, index) => {
+// 		words[index] = `<span><i>${words[index]}</i></span>`;
+// 	});
 
-	elem.innerHTML = words.join(' ');
+// 	elem.innerHTML = words.join(' ');
 
-	const children = document.querySelectorAll('span > i');
-	children.forEach((node, index) => {
-		node.style.animationDelay = `${index * .35}s`;
-	});
+// 	const children = document.querySelectorAll('span > i');
+// 	children.forEach((node, index) => {
+// 		node.style.animationDelay = `${index * .35}s`;
+// 	});
 
-});
+// });
 
 
 
